e79f44dbeb17733381d0edd85f0b94e27e7d8cbb
ilahiyye <ilahiyye@gmail.com>
2021-01-14 15:05:34 +0400

    'lost_password'

--- app/app/urls.py
+++ app/app/urls.py
@@ -2,6 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.contrib.auth import views as auth_views
+
 
 urlpatterns = [
     # path('', include("core.urls")),
@@ -10,6 +12,11 @@ urlpatterns = [
     path('', include('core.urls')),
     path('ckeditor/', include('ckeditor_uploader.urls')),
     path('pages/', include('django.contrib.flatpages.urls')),
+
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='accounts/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name="accounts/password_reset_confirm.html"), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='accounts/password_reset_complete.html'), name='password_reset_complete'),      
+    
 ]
 
 if settings.DEBUG:
--- app/core/urls.py
+++ app/core/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from . import views
 
+
 urlpatterns = [
     path('elanlar/new/', views.NewProductFormView.as_view(), name='new-product-add-page')
+    
 ]
