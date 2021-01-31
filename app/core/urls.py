bf9ff3ca1febef35e965f1c5c1d29d01c3db7f74
ughurguliyev <ugurguliyev24@gmail.com>
2021-01-31 17:30:31 +0400

    view and url problems resolved

--- app/core/urls.py
+++ app/core/urls.py
@@ -6,7 +6,6 @@ from core.views import UserProfilePageView, UserAccountSettingsView
 
 urlpatterns = [
     path('elanlar/new/', views.NewProductFormView.as_view(), name='new-product-add-page'),
-    path('post/<slug>', views.PostView.as_view(), name="post"),
     path('', views.HomePageView.as_view(), name='home-page'),
     path('elanlar/new/', views.NewProductFormView.as_view(), name='new-product-add-page'),
     path('card/', views.BasicTestView.as_view()),
