ee9312be5e8e5e975091ac6f004e630c5e38c339
ughurguliyev <ugurguliyev7@gmail.com>
2021-02-05 20:37:47 +0400

    changes commit

--- app/app/settings.py
+++ app/app/settings.py
@@ -95,10 +95,9 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ.get('POSTGRES_DB', "postgres"),
-        'USER': os.environ.get('POSTGRES_USER', "postgres"),
-        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', "1994"),
+        'NAME': os.environ.get('POSTGRES_DB', "app_db"),
+        'USER': os.environ.get('POSTGRES_USER', "app_user"),
+        'PASSWORD': os.environ.get('POSTGRES_PASSWORD', "d11ad0b3f014465b96e9c42639838c4"),
         'HOST': os.environ.get('POSTGRES_HOST', "localhost"),
         'PORT': os.environ.get('POSTGRES_PORT', 5432),
     }
