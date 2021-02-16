b78992e2f6502c47e5427298ac9141787d235962
İlahiye Musayeva <50544941+ilahiyye@users.noreply.github.com>
2021-02-16 15:01:38 +0400

    Update settings.py

--- app/app/settings.py
+++ app/app/settings.py
@@ -84,7 +84,6 @@ TEMPLATES = [
                 'django.contrib.messages.context_processors.messages',
                 
                 # Custom Processors
-                'core.custom_context_processor.subject_renderer', 
                 'core.context_processors.cities',
                 'core.context_processors.latest_products',
                 'core.context_processors.categories',
