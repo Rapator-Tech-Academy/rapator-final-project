139e46d6cd7804c9ccee2ba7b1b3065240c9d4de
Alasgar03 <sabanovmirelesger@gmail.com>
2021-02-18 20:52:54 +0400

    updated

--- app/core/urls.py
+++ app/core/urls.py
@@ -1,15 +1,13 @@
 from django.urls import path
 
 from . import views 
-from core.views import UserProfilePageView, UserAccountUpdateFormView
-
 
 urlpatterns = [
     path('elanlar/new/', views.NewProductFormView.as_view(), name='new-product-add-page'),
     path('', views.HomePageView.as_view(), name='home-page'),
     path('card/', views.BasicTestView.as_view()),
-    path('profile/', UserProfilePageView.as_view(), name='user-profile'),
-    path('profile-settings/', UserAccountSettingsView.as_view(), name='profile-settings'),
+    path('profile/', views.UserProfilePageView.as_view(), name='user-profile'),
+    path('profile-settings/', views.UserAccountUpdateFormView.as_view(), name='profile-settings'),
     path("elanlar/<slug>/", views.ProductView.as_view(),name="product-detail"),
     path("user_product_detail/<slug>/", views.ProductDetailView.as_view(),name="product-detail"),
     path('elanlar/<slug>/ad_edits', views.EditProductView.as_view(), name='product-edit-page'),
